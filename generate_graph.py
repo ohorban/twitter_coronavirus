5e4958d49a2665d92909f2f9c3bb9631d99745d3
Alex Horban <20ohorban@gmail.com>
2021-03-22 15:01:25 -0700

    trying matplotlib

---
+++ generate_graph.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_file',required=True)
+args = parser.parse_args()
+
+f = open(args.input_file, 'r')
+contents = f.readlines()
+
+labels = []
+sizes = []
+for entry in contents:
+    label = entry[:entry.index(':')-1]
+    size = entry[entry.index(':')+1:]
+    labels.append(label)
+    sizes.append(size)
+
+fig1, ax1 = plt.subplots()
+
+ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+ax1.axis('equal')
+plt.show()
